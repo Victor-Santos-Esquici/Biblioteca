4e9019dc60afd9d4782623cd6bcde02d787fd23b
Victor <vsepoa@gmail.com>
2017-05-04 21:47:38 -0300

    add

---
+++ src/com/br/entidade/Categoria.java
@@ -0,0 +1,12 @@
+package com.br.entidade;
+
+public class Categoria {
+
+	public Integer codCategoria;
+	public String descricao;
+	
+	public Categoria(Integer codCategoria, String descricao){
+		this.codCategoria = codCategoria;
+		this.descricao = descricao;
+	}
+}
